f8bdb429c0b2868f89633abd1af1f3fa514ef889
Paolo <paolo.ghidoni82@gmail.com>
2021-12-27 07:41:59 +0000

    Use the file system module to read and write files - relates #1
    * Used readFileSync() to synchronously read a file from
    the file system.
    * Used writeFileSync() to synchronously write a file to
    the file system.

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,11 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const hello = "Hello world";
-console.log(hello);
+// Reading a file from the file system
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+// Writing a file to the file system
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+
+console.log('File written!');
