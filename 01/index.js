5520c4a5fb1b54a08a718528247653a82e3ddde0
Paolo <paolo.ghidoni82@gmail.com>
2021-12-29 08:57:11 +0000

    Build a simple web server - relates #5
    * Used createServer() method to create a server.
    * Used listen() method to listen to incoming requests.

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,14 @@
 // require the fs module
 const fs = require('fs');
 
+// require the http module
+const http = require('http');
+
+
+
+/*------------------------------------*\
+  READING AND WRITING FILES
+\*------------------------------------*/
 
 
 /*** BLOCKING, synchronous way of reading ad writing files ***/
@@ -17,18 +25,37 @@ const fs = require('fs');
 
 
 
-/*** NON - BLOCKING, Asynchronous way of reading ad writing files ***/
-fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
-  if(error) return console.log('There was an error!');
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (error, data3) => {
-      console.log(data3);
-      fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, 'utf-8', error => {
-        console.log('The file has been written!')
-      });
-    });
-  });
+/*** NON - BLOCKING, Asynchronous way of reading ad writing files - CALLBACK HELL! ***/
+// fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
+//   if(error) return console.log('There was an error!');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (error, data3) => {
+//       console.log(data3);
+//       fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, 'utf-8', error => {
+//         console.log('The file has been written!')
+//       });
+//     });
+//   });
+// });
+
+// console.log('will read file');
+
+
+
+/*------------------------------------*\
+  BUILD A WEB SERVER
+\*------------------------------------*/
+
+
+// create the server and store it into a variable
+const server = http.createServer((request, response) => {
+  response.end("Hello from the server");
 });
 
-console.log('will read file');
+// listen to incoming requests to our server form the client
+server.listen(8000, '127.0.0.1', () => {
+  console.log('listening to requests on port 8000');
+}); 
+
+
